93603fd88cefbec1d594f497c189c8f485f4abbf
pletcher <charles.pletcher@gmail.com>
2017-03-09 23:23:44 -0500

    Add ability to look up short defs/glosses
    Right now, this will only work reliably with Greek
    headwords. It might be worth lemmatizing input to
    make the lookup more robust

---
+++ src/commands/gloss.js
@@ -0,0 +1,40 @@
+// monkey patch
+process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser
+
+const cheerio = require('cheerio')
+const request = require('request-promise')
+
+const defaults = {
+  response_type: 'in_channel'
+}
+
+const handler = (payload, res) => {
+  const { channel_name, text } = payload
+
+  return getShortDef(text).then(def => {
+    const msg = Object.assign(defaults, {
+      channel: channel_name,
+      text: def
+    })
+
+    res.set('content-type', 'application/json')
+
+    res.status(200).json(msg)
+  }).catch(err => {
+    res.send(404, `Something went wrong. Couldn't find definition for ${text}`)
+  })
+}
+
+module.exports = { handler, pattern: /gloss/i }
+
+function getShortDef(search_term) {
+  return request({
+    qs: {
+      search_term
+    },
+    transform(body) {
+      return cheerio.load(body)
+    },
+    uri: process.env.GLOSS_URI
+  }).then($ => $(`#${process.env.GLOSS_DEF_ID}`).text())
+}
